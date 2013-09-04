f771165e267f526e7a3a19ee86e003051a1e82eb
yorthehunter <boru.design@gmail.com>
2013-09-04 18:34:57 -0400

    Initial commit, new app created with Heroku in mind.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ProtoPom::Application.config.secret_key_base = '25cbc25bb7b89fd8835ce425823ecd57628285dd9ea4f6c82d6daa1171f0ff521cd2ccc9536f1af850f730a95a73eaa06c0397fb6ba9a893bb7b7a45b4a7bf22'
